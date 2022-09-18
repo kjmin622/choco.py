eeb20b76de0a9e05622d80809a59782b784ea9c3
tsalt1026 <moon856990@gmail.com>
2022-09-19 01:05:20 +0900

    stage1_wad

--- src/stage1/side.py
+++ src/stage1/side.py
@@ -1,8 +1,20 @@
 import os
 from constant import *
+import pygame
 
 class Side:
     def __init__(self,objectlist,image,path = None):
         self.objectlist = objectlist
-        self.imagepath = os.path.join(DIR_IMAGE,image)
+        self.image = pygame.image.load(os.path.join(DIR_IMAGE,image))
         self.path = path
+
+def turn_right(now):
+    next = now + 1
+    if next == 4:
+        next = 0
+    return next
+def turn_left(now):
+    before = now - 1
+    if before == -1:
+        before = 3
+    return before
