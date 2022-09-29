c5134d9fb21811f35c78c09db12467abd94d216a
kim-dong-woo <dongwoo12048@gmail.com>
2022-09-30 00:00:08 +0900

    stage3.py 함수화

--- src/stage3/stage3.py
+++ src/stage3/stage3.py
@@ -1,3 +1,4 @@
+from turtle import back
 import pygame
 import random
 import sys
@@ -5,30 +6,13 @@ import os
 from timer import *
 
 pygame.init()
-
+pygame.mixer.init()
 clock = pygame.time.Clock()
-
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-DIR_PATH = os.path.dirname(__file__)
-DIR_IMAGE = os.path.join(DIR_PATH, 'image')
-
-current_time = 0
-
-mouse_pos = (0,0)
-
-password = [0,1,2,3,4,5,6,7,8,9]
-random.shuffle(password)
-password = password[:5]
-
-button_list = []
-button_click_list = []
+screen = pygame.display.set_mode((1024,768))
 
 class Button:
     
-    def __init__(self, x_pos, y_pos):
+    def __init__(self, x_pos, y_pos, screen):
         self.image = pygame.image.load(os.path.join(DIR_IMAGE,'button.png')).convert()
         self.rect = self.image.get_rect()
         self.size = self.image.get_rect().size
@@ -38,9 +22,10 @@ class Button:
         self.y_pos = y_pos
         self.rect.left = self.x_pos
         self.rect.top = self.y_pos
+        self.screen = screen
         
     def button_draw(self):
-        screen.blit(self.image, (self.x_pos, self.y_pos))
+        self.screen.blit(self.image, (self.x_pos, self.y_pos))
 
     def button_press(self):
         self.image = pygame.image.load(os.path.join(DIR_IMAGE,'button_press.png')).convert()
@@ -78,14 +63,15 @@ def show_password(level):#현재 레벨
     while not delay_timer.is_done():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
+                sys.exit()
             if event.type == pygame.MOUSEBUTTONUP:
-                    mouse_pos = event.pos
-                    for i in range(len(button_list)):
-                        if button_list[i].button_is_clicked(mouse_pos):
-                            button_click_list.append(i)
-                            button_list[i].button_press()
-                            pygame.display.update()
+                mouse_pos = event.pos
+                for i in range(len(button_list)):
+                    if button_list[i].button_is_clicked(mouse_pos):
+                        button_click_list.append(i)
+                        button_list[i].button_press()
+                        pygame.display.update()
         
         if button_click_list == password[:level]:
             break
@@ -97,202 +83,195 @@ def show_password(level):#현재 레벨
 def is_password_correct(level):
     return button_click_list == password[:level]
 
-for i in range(10):
-    button_list.append(Button(104 + (i%5)*184, 203 + (i//5)*283))
+def init(screen, clock):
 
-game_font = pygame.font.Font(None,40)
+    global password, button_list, button_click_list, mouse_pos, SCREEN_WIDTH, SCREEN_HEIGHT, DIR_IMAGE
 
-pygame.display.set_caption("ChoCo")
+    DIR_PATH = os.path.dirname(__file__)
+    DIR_IMAGE = os.path.join(DIR_PATH, 'image')
 
-background = pygame.image.load(os.path.join(DIR_IMAGE,'floor.png')).convert()
+    SCREEN_WIDTH = 1024
+    SCREEN_HEIGHT = 768
 
-character = pygame.image.load(os.path.join(DIR_IMAGE,'character_1.png')).convert()
-character_size = character.get_rect().size
-character_width = character_size[0]
-character_height = character_size[1]
-character_x_pos = SCREEN_WIDTH/2
-character_y_pos = SCREEN_HEIGHT - character_height
+    mouse_pos = (0,0)
 
-dogs = []
-for _ in range(4):
-    dog = pygame.image.load(os.path.join(DIR_IMAGE,'dog.png')).convert()
-    dog_size = dog.get_rect().size
-    dog_width = dog_size[0]
-    dog_height = dog_size[1]
-    dog_x_pos = random.randint(0,SCREEN_WIDTH)
-    dog_y_pos = random.randint(0,SCREEN_HEIGHT)
-    dogs.append([dog,dog_width,dog_height,dog_x_pos,dog_y_pos])
-    
-portal = pygame.image.load(os.path.join(DIR_IMAGE,'portal.png')).convert()
-portal_size = portal.get_rect().size
-portal_width = portal_size[0]
-portal_height = portal_size[1]
-portal_x_pos = SCREEN_WIDTH - portal_width
-portal_y_pos = 0
+    password = [0,1,2,3,4,5,6,7,8,9]
+    random.shuffle(password)
+    password = password[:5]
 
-lights = []
-for i in range(5):
-    light = pygame.image.load(os.path.join(DIR_IMAGE,'light.png')).convert()
-    light_x_pos = 432 + i*30
-    light_y_pos = 50
-    lights.append([light,light_x_pos,light_y_pos])
+    button_list = []
+    button_click_list = []
 
-to_x = 0
-to_y = 0
+    to_x = 0
+    to_y = 0
 
-gameover = False
-running = True
-enter = False
+    background = pygame.image.load(os.path.join(DIR_IMAGE,'floor.png')).convert()
 
-print(password)
+    character = pygame.image.load(os.path.join(DIR_IMAGE,'character_1.png')).convert()
+    character_size = character.get_rect().size
+    character_width = character_size[0]
+    character_height = character_size[1]
+    character_x_pos = SCREEN_WIDTH/2
+    character_y_pos = SCREEN_HEIGHT - character_height
 
-while running:
-    screen.blit(background,(0,0))
-    dt = clock.tick(60)
-    if not enter:#포탈 입장 전
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    to_x -= 1
-                elif event.key == pygame.K_d:
-                    to_x += 1
-                elif event.key == pygame.K_w:
-                    to_y -= 1
-                elif event.key == pygame.K_s:
-                     to_y += 1
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a or event.key == pygame.K_d:
-                    to_x = 0
-                elif event.key == pygame.K_w or event.key == pygame.K_s:
-                    to_y = 0
-        if character_x_pos < 0:
-            character_x_pos = 0
-        elif character_x_pos > SCREEN_WIDTH - character_width:
-            character_x_pos = SCREEN_WIDTH - character_width
-        if character_y_pos < 0:
-            character_y_pos = 0
-        elif character_y_pos > SCREEN_HEIGHT - character_height:
-            character_y_pos = SCREEN_HEIGHT - character_height
-
-        character_x_pos += to_x*dt
-        character_y_pos += to_y*dt
-
-        character_rect = character.get_rect()
-        character_rect.left = character_x_pos
-        character_rect.top = character_y_pos
-
-        portal_rect = portal.get_rect()
-        portal_rect.left = portal_x_pos
-        portal_rect.top = portal_y_pos
-
-        for dog in dogs:
-            if dog[3] < 0:
-                dog[3] = 0
-            elif dog[3] > SCREEN_WIDTH - dog[1]:
-                dog[3] = SCREEN_WIDTH - dog[1]
-            if dog[4] < 0:
-                dog[4] = 0
-            elif dog[4] > SCREEN_HEIGHT - dog[2]:
-                dog[4] = SCREEN_HEIGHT - dog[2]
-            dir = [[1,0],[-1,0],[0,1],[0,-1]]
-            now_dir = random.choice(dir)
-            dog[3] += 3*now_dir[0]
-            dog[4] += 3*now_dir[1]
-
-            dog_rect = dog[0].get_rect()
-            dog_rect.left = dog[3]
-            dog_rect.top = dog[4]
-
-            if character_rect.colliderect(dog_rect):
-                running = False
-                gameover = True
+    dogs = []
+    for _ in range(4):
+        dog = pygame.image.load(os.path.join(DIR_IMAGE,'dog.png')).convert()
+        dog_size = dog.get_rect().size
+        dog_width = dog_size[0]
+        dog_height = dog_size[1]
+        dog_x_pos = random.randint(0,SCREEN_WIDTH)
+        dog_y_pos = random.randint(0,SCREEN_HEIGHT)
+        dogs.append([dog,dog_width,dog_height,dog_x_pos,dog_y_pos])
         
-        if character_rect.colliderect(portal_rect):
-            background = pygame.image.load(os.path.join(DIR_IMAGE,'next.png')).convert()
-            dogs = []
-            enter = True
-        screen.blit(character,(character_x_pos,character_y_pos))
-        screen.blit(portal,(portal_x_pos,portal_y_pos))
-        for dog in dogs:
-            screen.blit(dog[0],(dog[3],dog[4]))
+    portal = pygame.image.load(os.path.join(DIR_IMAGE,'portal.png')).convert()
+    portal_size = portal.get_rect().size
+    portal_width = portal_size[0]
+    portal_x_pos = SCREEN_WIDTH - portal_width
+    portal_y_pos = 0
+
+    lights = []
+    for i in range(5):
+        light = pygame.image.load(os.path.join(DIR_IMAGE,'light.png')).convert()
+        light_x_pos = 432 + i*30
+        light_y_pos = 50
+        lights.append([light,light_x_pos,light_y_pos])
+
+    for i in range(10):
+        button_list.append(Button(104 + (i%5)*184, 203 + (i//5)*283, screen))
+
+    return {'screen' : screen, 'clock' : clock, 'background' : background, 'to_x' : to_x, 'to_y' : to_y, 'character' : character, 'character_width' : character_width, 'character_height' : character_height, 'character_x_pos' : character_x_pos, 'character_y_pos' : character_y_pos, 'dogs' : dogs, 'portal' : portal, 'portal_x_pos' : portal_x_pos, 'portal_y_pos' : portal_y_pos, 'lights' : lights}
+
+def main(param):
+    screen = param['screen']; clock = param['clock']; background = param['background']; to_x = param['to_x']; to_y = param['to_y']; character = param['character']; character_width = param['character_width']; character_height = param['character_height']; character_x_pos = param['character_x_pos']; character_y_pos = param['character_y_pos']; dogs = param['dogs']; portal = param['portal']; portal_x_pos = param['portal_x_pos']; portal_y_pos = param['portal_y_pos']; lights = param['lights']
+    enter = False
+    while True:
+        screen.blit(background,(0,0))
+        dt = clock.tick(60)
+        if not enter:#포탈 입장 전
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        to_x -= 1
+                    elif event.key == pygame.K_d:
+                        to_x += 1
+                    elif event.key == pygame.K_w:
+                        to_y -= 1
+                    elif event.key == pygame.K_s:
+                        to_y += 1
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a or event.key == pygame.K_d:
+                        to_x = 0
+                    elif event.key == pygame.K_w or event.key == pygame.K_s:
+                        to_y = 0
+            #캐릭터 화면끝 안넘어가기
+            if character_x_pos < 0:
+                character_x_pos = 0
+            elif character_x_pos > SCREEN_WIDTH - character_width:
+                character_x_pos = SCREEN_WIDTH - character_width
+            if character_y_pos < 0:
+                character_y_pos = 0
+            elif character_y_pos > SCREEN_HEIGHT - character_height:
+                character_y_pos = SCREEN_HEIGHT - character_height
+
+            character_x_pos += to_x*dt
+            character_y_pos += to_y*dt
+
+            character_rect = character.get_rect()
+            character_rect.left = character_x_pos
+            character_rect.top = character_y_pos
+
+            portal_rect = portal.get_rect()
+            portal_rect.left = portal_x_pos
+            portal_rect.top = portal_y_pos
+
+            for dog in dogs:
+                if dog[3] < 0:
+                    dog[3] = 0
+                elif dog[3] > SCREEN_WIDTH - dog[1]:
+                    dog[3] = SCREEN_WIDTH - dog[1]
+                if dog[4] < 0:
+                    dog[4] = 0
+                elif dog[4] > SCREEN_HEIGHT - dog[2]:
+                    dog[4] = SCREEN_HEIGHT - dog[2]
+                dir = [[1,0],[-1,0],[0,1],[0,-1]]
+                now_dir = random.choice(dir)
+                dog[3] += 3*now_dir[0]
+                dog[4] += 3*now_dir[1]
+
+                dog_rect = dog[0].get_rect()
+                dog_rect.left = dog[3]
+                dog_rect.top = dog[4]
+
+                if character_rect.colliderect(dog_rect):
+                    main(param)
             
-    else:#포탈 입장 후
+            if character_rect.colliderect(portal_rect):
+                background = pygame.image.load(os.path.join(DIR_IMAGE,'next.png')).convert()
+                dogs = []
+                enter = True
 
-        for button in button_list:
-            button.button_draw()
+            screen.blit(character,(character_x_pos,character_y_pos))
+            screen.blit(portal,(portal_x_pos,portal_y_pos))
+            for dog in dogs:
+                screen.blit(dog[0],(dog[3],dog[4]))
+                
+        else:#포탈 입장 후
 
-        for light in lights:
-            screen.blit(light[0],(light[1],light[2]))
+            for button in button_list:
+                button.button_draw()
 
-        pygame.display.update()
+            for light in lights:
+                screen.blit(light[0],(light[1],light[2]))
 
-        show_password(1)
-        if(is_password_correct(1)):
-            lights[0][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
-            screen.blit(lights[0][0],(lights[0][1],lights[0][2]))
             pygame.display.update()
-            button_click_list = []
-            show_password(2)
-            if(is_password_correct(2)):
-                lights[1][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
-                screen.blit(lights[1][0],(lights[1][1],lights[1][2]))
+
+            show_password(1)
+            if(is_password_correct(1)):
+                lights[0][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
+                screen.blit(lights[0][0],(lights[0][1],lights[0][2]))
                 pygame.display.update()
-                button_click_list = []
-                show_password(3)
-                if(is_password_correct(3)):
-                    lights[2][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
-                    screen.blit(lights[2][0],(lights[2][1],lights[2][2]))
+                button_click_list.clear()
+                show_password(2)
+                if(is_password_correct(2)):
+                    lights[1][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
+                    screen.blit(lights[1][0],(lights[1][1],lights[1][2]))
                     pygame.display.update()
-                    button_click_list = []
-                    show_password(4)
-                    if(is_password_correct(4)):
-                        lights[3][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
-                        screen.blit(lights[3][0],(lights[3][1],lights[3][2]))
+                    button_click_list.clear()
+                    show_password(3)
+                    if(is_password_correct(3)):
+                        lights[2][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
+                        screen.blit(lights[2][0],(lights[2][1],lights[2][2]))
                         pygame.display.update()
-                        button_click_list = []
-                        show_password(5)
-                        if(is_password_correct(5)):
-                            lights[4][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
-                            screen.blit(lights[4][0],(lights[4][1],lights[4][2]))
+                        button_click_list.clear()
+                        show_password(4)
+                        if(is_password_correct(4)):
+                            lights[3][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
+                            screen.blit(lights[3][0],(lights[3][1],lights[3][2]))
                             pygame.display.update()
-                            clear_timer = Timer(1)
-                            clear_timer.start()
-                            msg = game_font.render("Clear",True,(255,255,0)) # 노란색
-                            msg_rect = msg.get_rect(center=(int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2)))
-                            while not clear_timer.is_done():
-                                screen.blit(msg,msg_rect)
+                            button_click_list.clear()
+                            show_password(5)
+                            if(is_password_correct(5)):
+                                lights[4][0] = pygame.image.load(os.path.join(DIR_IMAGE,'light_clear.png')).convert()
+                                screen.blit(lights[4][0],(lights[4][1],lights[4][2]))
                                 pygame.display.update()
-                            running = False
+                                return True
+                            else:
+                                main(param)
                         else:
-                            running = False
-                            gameover = True
+                            main(param)
                     else:
-                        running = False
-                        gameover = True
+                        main(param)
                 else:
-                    running = False
-                    gameover = True
+                    main(param)
             else:
-                running = False
-                gameover = True
-        else:
-            running = False
-            gameover = True
+                main(param)
 
-    pygame.display.update()
-
-if gameover:#게임오버 되었을 경우
-    gameover_timer = Timer(1)
-    gameover_timer.start()
-    msg = game_font.render("Game Over",True,(255,255,0)) # 노란색
-    msg_rect = msg.get_rect(center=(int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2)))
-    while not gameover_timer.is_done():
-        screen.blit(msg,msg_rect)
         pygame.display.update()
 
-pygame.quit()
-sys.exit()
+main(init(screen, clock))
