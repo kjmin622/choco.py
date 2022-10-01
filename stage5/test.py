f365389a045977972c01f9cc9e01771816a12985
SeokHyeon <hyun_21@naver.com>
2022-10-02 03:30:22 +0900

    stage5

--- stage5/test.py
+++
@@ -1,9 +0,0 @@
-import stage5
-import pygame
-
-pad_size = (1024,768)
-
-pygame.init()
-screen = pygame.display.set_mode(pad_size)
-clock = pygame.time.Clock()
-stage5.main(stage5.init(screen,clock))
